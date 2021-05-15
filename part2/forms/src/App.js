2365419ff4761341501ceb48a46455bc0ead07dc
hjsith <hasithjayasekera@gmail.com>
2021-05-15 23:47:44 +1000

    Changed variable name

--- part2/forms/src/App.js
+++ part2/forms/src/App.js
@@ -44,7 +44,7 @@ const App = () => {
       .then((returnedNote) => {
         setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
       })
-      .catch((_) => {
+      .catch((error) => {
         alert(`the note '${note.content}' was already deleted from server`);
         setNotes(notes.filter((n) => n.id !== id));
       });
