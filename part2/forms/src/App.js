78c23072dd51755d018a1a4cfa61d447e5f2cd7a
hjsith <hasithjayasekera@gmail.com>
2021-07-13 21:59:05 +1000

    Inline styles

--- part2/forms/src/App.js
+++ part2/forms/src/App.js
@@ -3,6 +3,22 @@ import noteService from "./services/notes";
 import Note from "./components/Note";
 import Notification from "./components/Notification";
 
+const Footer = () => {
+  const footerStyle = {
+    color: "green",
+    fontStyle: "italic",
+    fontSize: 16,
+  };
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>
+        Note app, Department of Computer Science, University of Helsinki 2021
+      </em>
+    </div>
+  );
+};
+
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
@@ -81,6 +97,7 @@ const App = () => {
         <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
+      <Footer />
     </div>
   );
 };
